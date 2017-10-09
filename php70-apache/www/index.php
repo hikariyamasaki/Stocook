0d8c5ff8395200aacbffcab26e69f986f00caedd
Hikari <hyamasaki0101@gmail.com>
2017-10-09 15:18:33 +0900

    トップページ編集のブランチを作成しました。

--- php70-apache/www/index.php
+++ php70-apache/www/index.php
@@ -4,10 +4,11 @@
   <meta charset="utf-8">
   <title></title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link rel="stylesheet" type="text/css" href="index.css">
  </head>
 <body>
   <div class="main">
-    <h1>Stocook</h1>
+    <h1>Stocook!</h1>
     <div class="select">
       <p>冷蔵庫の食材を見る</p>
        <a class="btn meat" href="./meat.html" style="text-decoration:none;">お肉</a>
